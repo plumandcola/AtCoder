8e1e9d9525e85d9bc13f0c1adf4c8662e908e990
plumandcola <namite0207@gmail.com>
2026-05-15 21:42:23 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc215/abc215_e.py
+++ ABC/abc215/abc215_e.py
@@ -8,17 +8,18 @@ dp[i][b][j] := i回目のコンテストまでで、
 最後に出場したコンテストがj種類目である選び方の総数
 """
 dp[0][0][0] = 1
+mod = 998244353
 for i in range(N):
     n = ord(S[i]) - ord('A')
     for b in range(1 << 10):
         for j in range(10):
-            dp[i+1][b][j] = (dp[i+1][b][j] + dp[i][b][j]) % 998244353 #参加しない
+            dp[i+1][b][j] = (dp[i+1][b][j] + dp[i][b][j]) % mod #参加しない
             if b >> n & 1 == 0 or j == n: #参加する条件を満たしている時
-                dp[i+1][b | (1 << n)][n] = (dp[i+1][b | (1 << n)][n] + dp[i][b][j]) % 998244353 #参加する
+                dp[i+1][b | (1 << n)][n] = (dp[i+1][b | (1 << n)][n] + dp[i][b][j]) % mod #参加する
 
 ans = 0
 for b in range(1 << 10):
     for j in range(10):
-        ans = (ans + dp[N][b][j]) % 998244353
+        ans = (ans + dp[N][b][j]) % mod
 
-print((ans - 1) % 998244353)
+print((ans - 1) % mod)
