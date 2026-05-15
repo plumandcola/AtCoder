afa37598ccc7b3168a3616f93597f16927ec0154
plumandcola <namite0207@gmail.com>
2026-05-15 21:49:25 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc248/abc248_c TLE.py
+++ ABC/abc248/abc248_c TLE.py
@@ -6,7 +6,7 @@ def matrix_product(A, B):
     for i in range(m):
         for j in range(l):
             for k in range(n):
-                AB[i][j] = (AB[i][j] + A[i][k] * B[k][j]) % 998244353
+                AB[i][j] = (AB[i][j] + A[i][k] * B[k][j]) % mod
     return AB
 
 
@@ -15,6 +15,7 @@ N, M, K = map(int, input().split())
 ans = [[0] for _ in range(K+1)]
 ans[0][0] = 1
 transformation_matrix = [[1 if 1 <= i-j <= M else 0 for j in range(K+1)] for i in range(K+1)]
+mod = 998244353
 
 while N:
     if N & 1 == 1:
@@ -22,4 +23,4 @@ while N:
     transformation_matrix = matrix_product(transformation_matrix, transformation_matrix)
     N >>= 1
 
-print(sum(ans[i][0] for i in range(K+1)) % 998244353)
+print(sum(ans[i][0] for i in range(K+1)) % mod)
