98decaae7d1026802d4ecd687ef01fb6b1f127b2
plumandcola <namite0207@gmail.com>
2026-05-15 21:50:02 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc248/abc248_c.py
+++ ABC/abc248/abc248_c.py
@@ -2,8 +2,9 @@ N, M, K = map(int, input().split())
 
 dp = [[0] * (K+1) for _ in range(N+1)]
 dp[0][0] = 1
+mod = 998244353
 for i in range(N):
     for j in range(K+1):
-        dp[i+1][j] = sum(dp[i][max(0,j-M):j]) % 998244353
+        dp[i+1][j] = sum(dp[i][max(0,j-M):j]) % mod
 
-print(sum(dp[N]) % 998244353)
+print(sum(dp[N]) % mod)
