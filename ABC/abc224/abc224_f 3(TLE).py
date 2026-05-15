9c9832d2e8d863f20747c74ce603e9903a562907
plumandcola <namite0207@gmail.com>
2026-05-15 21:43:30 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc224/abc224_f 3(TLE).py
+++ ABC/abc224/abc224_f 3(TLE).py
@@ -3,10 +3,11 @@ import numpy
 S = input()
 
 ans = numpy.array([[0], [int(S[0])], [1]])
+mod = 998244353
 for i in range(1, len(S)):
     ans = numpy.array([[2, 1, 0], [0, 10, 2 * int(S[i])], [0, 0, 2]]) @ ans
-    ans %= 998244353
+    ans %= mod
 ans = numpy.array([[1, 1, 0]]) @ ans
-ans %= 998244353
+ans %= mod
 
 print(ans[0][0])
