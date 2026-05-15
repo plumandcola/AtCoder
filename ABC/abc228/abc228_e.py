043eb03546df98061f5efdf72043ce849d2f5a3c
plumandcola <namite0207@gmail.com>
2026-05-15 21:43:51 +0900

    Refactor modulus variable for clarity and consistency

--- ABC/abc228/abc228_e.py
+++ ABC/abc228/abc228_e.py
@@ -1,6 +1,7 @@
 N, K, M = map(int, input().split())
+mod = 998244353
 
-if M % 998244353 == 0:
+if M % mod == 0:
     print(0)
 else:
-    print(pow(M, pow(K, N, 998244352), 998244353))
+    print(pow(M, pow(K, N, mod - 1), mod))
