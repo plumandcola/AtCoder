e84b895296d4c153b7c4889e0bb45d42cab6880e
plumandcola <namite0207@gmail.com>
2026-05-15 21:54:38 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc224/abc224_f 2.py
+++ ABC/abc224/abc224_f 2.py
@@ -6,13 +6,14 @@ def matrix_product(A, B):
     for i in range(m):
         for j in range(l):
             for k in range(n):
-                AB[i][j] = (AB[i][j] + A[i][k] * B[k][j]) % 998244353
+                AB[i][j] = (AB[i][j] + A[i][k] * B[k][j]) % mod
     return AB
 
 
 S = input()
 
 ans = [[0], [int(S[0])], [1]]
+mod = 998244353
 for i in range(1, len(S)):
     ans = matrix_product([[2, 1, 0], [0, 10, 2 * int(S[i])], [0, 0, 2]], ans)
 ans = matrix_product([[1, 1, 0]], ans)
