94e48a63be0cd555e04fc4f8f5f14e34049fe01d
plumandcola <namite0207@gmail.com>
2026-05-15 21:42:42 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc220/abc220_d.py
+++ ABC/abc220/abc220_d.py
@@ -3,10 +3,11 @@ A = list(map(int, input().split()))
 
 dp = [[0] * 10 for _ in range(N)]
 dp[0][A[0]] = 1
+mod = 998244353
 for i in range(N-1):
     for j in range(10):
-        dp[i+1][(j + A[i+1]) % 10] = (dp[i+1][(j + A[i+1]) % 10] + dp[i][j]) % 998244353
-        dp[i+1][(j * A[i+1]) % 10] = (dp[i+1][(j * A[i+1]) % 10] + dp[i][j]) % 998244353
+        dp[i+1][(j + A[i+1]) % 10] = (dp[i+1][(j + A[i+1]) % 10] + dp[i][j]) % mod
+        dp[i+1][(j * A[i+1]) % 10] = (dp[i+1][(j * A[i+1]) % 10] + dp[i][j]) % mod
 
 for K in range(10):
     print(dp[N-1][K])
