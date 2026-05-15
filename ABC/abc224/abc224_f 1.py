e8ad4eb50f9bb63e46912a0c9a90a97790bc098d
plumandcola <namite0207@gmail.com>
2026-05-15 21:43:11 +0900

    Replace hardcoded modulus with variable 'mod'

--- ABC/abc224/abc224_f 1.py
+++ ABC/abc224/abc224_f 1.py
@@ -2,18 +2,19 @@ S = input()
 
 ans = 0
 n = len(S)
+mod = 998244353
 for i in range(n):
-    ans_i = pow(10, n-i-1, 998244353) #"+"がないときの数字
+    ans_i = pow(10, n-i-1, mod) #"+"がないときの数字
     
     if i < n-1:
-        ans_i += pow(2, n-i-2, 998244353) * (pow(5, n-i-1, 998244353) - 1) * pow(4, 998244351, 998244353)
+        ans_i += pow(2, n-i-2, mod) * (pow(5, n-i-1, mod) - 1) * pow(4, mod - 2, mod)
         #後ろの"+"の有無や桁数などを考慮して導かれた等比数列の和 2^(n-i-2) * (5^(n-i-1) - 1) / 4
     
-    ans_i %= 998244353
+    ans_i %= mod
 
-    ans_i = ans_i * int(S[i]) * pow(2, i, 998244353) % 998244353
+    ans_i = ans_i * int(S[i]) * pow(2, i, mod) % mod
     #i+1文字目の数字 × それまでの"+"の有無
     
-    ans = (ans + ans_i) % 998244353
+    ans = (ans + ans_i) % mod
 
 print(ans)
