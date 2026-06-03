f233534a49c26efdfaf86782bc9500a9010b071f
plumandcola <namite0207@gmail.com>
2026-06-03 22:53:42 +0900

    Refactor union-find functions into UnionFind class

--- ABC/abc235/abc235_e 1.py
+++ ABC/abc235/abc235_e 1.py
@@ -1,28 +1,34 @@
-def find(x: int) -> int:
-    """要素xが属するグループの根を返す"""
-    if parents[x] < 0: #根
-        return x
-    else:
-        parents[x] = find(parents[x]) #経路圧縮
-        return parents[x]
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+        """各要素の親要素の番号を格納するリスト、要素が根（ルート）の場合は-(そのグループの要素数)を格納する"""
+    
+    def find(self, x: int) -> int:
+        """要素xが属するグループの根を返す"""
+        if self.parents[x] < 0: #根
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x]) #経路圧縮
+            return self.parents[x]
 
-def union(x: int, y: int):
-    """要素xが属するグループと要素yが属するグループとを併合する"""
-    x = find(x)
-    y = find(y)
-    
-    if x == y:
-        return
-    
-    if parents[x] > parents[y]:
-        x, y = y, x
-    
-    parents[x] += parents[y]
-    parents[y] = x
+    def union(self, x: int, y: int):
+        """要素xが属するグループと要素yが属するグループとを併合する"""
+        x = self.find(x)
+        y = self.find(y)
+        
+        if x == y:
+            return
+        
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
 
-def same(x: int, y: int) -> bool:
-    """要素x, yが同じグループに属するかどうかを返す"""
-    return find(x) == find(y)
+    def same(self, x: int, y: int) -> bool:
+        """要素x, yが同じグループに属するかどうかを返す"""
+        return self.find(x) == self.find(y)
 
 
 N, M, Q = map(int, input().split())
@@ -39,13 +45,12 @@ for i in range(Q):
 edges.sort()
 
 ans = [False] * Q
-parents = [-1] * N
-"""各要素の親要素の番号を格納するリスト、要素が根（ルート）の場合は-(そのグループの要素数)を格納する"""
+uf = UnionFind(N)
 for c, i, a, b in edges:
     if i == -1:
-        union(a, b)
+        uf.union(a, b)
     else:
-        ans[i] = not same(a, b)
+        ans[i] = not uf.same(a, b)
 
 for i in range(Q):
     print("Yes" if ans[i] else "No")
