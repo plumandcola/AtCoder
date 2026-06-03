e4c41d54926a6185b1a0201a4f7bb1c2bd5872d4
plumandcola <namite0207@gmail.com>
2026-06-03 22:51:04 +0900

    Refactor union-find functions into UnionFind class

--- ABC/abc229/abc229_e.py
+++ ABC/abc229/abc229_e.py
@@ -1,26 +1,32 @@
-def find(x: int) -> int:
-    """要素xが属するグループの根を返す"""
-    if parents[x] < 0: #根
-        return x
-    else:
-        parents[x] = find(parents[x]) #経路圧縮
-        return parents[x]
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+        """各要素の親要素の番号を格納するリスト、要素が根（ルート）の場合は-(そのグループの要素数)を格納する"""
+    
+    def find(self, x: int) -> int:
+        """要素xが属するグループの根を返す"""
+        if self.parents[x] < 0: #根
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x]) #経路圧縮
+            return self.parents[x]
 
-def union(x: int, y: int) -> int:
-    """要素xが属するグループと要素yが属するグループとを併合する"""
-    x = find(x)
-    y = find(y)
-    
-    if x == y:
-        return ans_i
-    
-    if parents[x] > parents[y]:
-        x, y = y, x
-    
-    parents[x] += parents[y]
-    parents[y] = x
-    
-    return ans_i - 1
+    def union(self, x: int, y: int) -> int:
+        """要素xが属するグループと要素yが属するグループとを併合する"""
+        x = self.find(x)
+        y = self.find(y)
+        
+        if x == y:
+            return ans_i
+        
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+        return ans_i - 1
 
 
 N, M = map(int, input().split())
@@ -30,8 +36,7 @@ for _ in range(M):
     A, B = map(int, input().split())
     g[A-1].append(B-1)
 
-parents = [-1] * N
-"""各要素の親要素の番号を格納するリスト、要素が根（ルート）の場合は-(そのグループの要素数)を格納する"""
+uf = UnionFind(N)
 
 ans = [0] #何もないところに、頂点N-1から逆順に1まで追加していく
 ans_i = 0
@@ -39,7 +44,7 @@ for i in range(N-1, 0, -1):
     ans_i += 1
 
     for j in g[i]:
-        ans_i = union(i, j)
+        ans_i = uf.union(i, j)
     
     ans.append(ans_i)
 
