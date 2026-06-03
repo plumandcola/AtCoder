01984ad3c95bb1a1c892c320f0ed74fe78c32ae6
plumandcola <namite0207@gmail.com>
2026-06-03 22:46:50 +0900

    Refactor union-find functions into UnionFind class

--- ABC/abc214/abc214_d.py
+++ ABC/abc214/abc214_d.py
@@ -1,30 +1,36 @@
 import heapq
 
-def find(x: int) -> int:
-    """要素xが属するグループの根を返す"""
-    if parents[x] < 0: #根
-        return x
-    else:
-        parents[x] = find(parents[x]) #経路圧縮
-        return parents[x]
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+        """各要素の親要素の番号を格納するリスト、要素が根（ルート）の場合は-(そのグループの要素数)を格納する"""
+    
+    def find(self, x: int) -> int:
+        """要素xが属するグループの根を返す"""
+        if self.parents[x] < 0: #根
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x]) #経路圧縮
+            return self.parents[x]
 
-def union(x: int, y: int):
-    """要素xが属するグループと要素yが属するグループとを併合する"""
-    x = find(x)
-    y = find(y)
-    
-    if x == y:
-        return
-    
-    if parents[x] > parents[y]:
-        x, y = y, x
-    
-    parents[x] += parents[y]
-    parents[y] = x
+    def union(self, x: int, y: int):
+        """要素xが属するグループと要素yが属するグループとを併合する"""
+        x = self.find(x)
+        y = self.find(y)
+        
+        if x == y:
+            return
+        
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
 
-def size(x: int) -> int:
-    """要素xが属するグループのサイズ（要素数）を返す"""
-    return -parents[find(x)]
+    def size(self, x: int) -> int:
+        """要素xが属するグループのサイズ（要素数）を返す"""
+        return - self.parents[self.find(x)]
 
 
 N = int(input())
@@ -35,11 +41,10 @@ for _ in range(N-1):
     heapq.heappush(edges, (w, u-1, v-1))
 
 ans = 0
-parents = [-1] * N
-"""各要素の親要素の番号を格納するリスト、要素が根（ルート）の場合は-(そのグループの要素数)を格納する"""
+uf = UnionFind(N)
 for _ in range(N-1):
     w, u, v = heapq.heappop(edges)
-    ans += w * (size(u) * size(v))
-    union(u, v)
+    ans += w * (uf.size(u) * uf.size(v))
+    uf.union(u, v)
 
 print(ans)
