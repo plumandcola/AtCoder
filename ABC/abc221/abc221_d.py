08b2b241c5383c11f1fe17d9fac22405aa10d0f8
plumandcola <namite0207@gmail.com>
2026-06-19 11:34:49 +0900

    Replace imos with events for event counting

--- ABC/abc221/abc221_d.py
+++ ABC/abc221/abc221_d.py
@@ -1,17 +1,17 @@
-imos = []
+events = []
 
 N = int(input())
 for _ in range(N):
     A, B = map(int, input().split())
-    imos.append((A, 1))
-    imos.append((A + B, -1))
+    events.append((A, 1))
+    events.append((A + B, -1))
 
-imos.sort()
+events.sort()
 
 ans = [0] * (N+1)
 s = 0 #ログインしている人数
-for i in range(2*N-1):
-    s += imos[i][1]
-    ans[s] += imos[i+1][0] - imos[i][0]
+for i in range(2*N - 1):
+    s += events[i][1]
+    ans[s] += events[i+1][0] - events[i][0]
 
 print(*ans[1:])
