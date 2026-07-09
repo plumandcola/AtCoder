28bb65954a96a1a708ccd4460f760206bc35cd42
plumandcola <namite0207@gmail.com>
2026-07-10 00:19:20 +0900

    Optimize search conditions in abc246_d.py
    Refactor binary search logic and update conditions for better performance.

--- ABC/abc246/abc246_d.py
+++ ABC/abc246/abc246_d.py
@@ -6,16 +6,16 @@ N = int(input())
 
 ans = float("inf")
 a = 0
-while 4*a*a*a <= 10 ** 18:
-    l = a
+while 4*a*a*a < ans:
+    l = a - 1
     r = 1000001
-    while l != r:
-        mid = (l+r) // 2
+    while r - l > 1:
+        mid = (l + r) // 2
         if f(a, mid) < N:
-            l = mid + 1
+            l = mid
         else:
             r = mid
-    ans = min(ans, f(a, l))
+    ans = min(ans, f(a, r))
     a += 1
 
 print(ans)
