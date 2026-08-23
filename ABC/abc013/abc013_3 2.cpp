c9ebdd8f2c23e5013b8e7a087eeb76596df2d95d
plumandcola <namite0207@gmail.com>
2026-08-23 22:59:23 +0900

    Refactor code for improved performance and clarity

--- ABC/abc013/abc013_3 2.cpp
+++ ABC/abc013/abc013_3 2.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 
 int main() {
-    // 40点解法
-    int N, H, A, B, C, D, E;
+    // 30点解法
+    long long N, H, A, B, C, D, E, INF = 1LL << 62;
     cin >> N >> H >> A >> B >> C >> D >> E;
 
-    vector<map<int, long long>> dp(N+1);
+    vector<vector<long long>> dp(N+1, vector<long long>(H + N*B + 1, INF));
     dp[0][H] = 0;
-    for (int i = 0; i < N; i++) {
-        // ここから書く
+    for (long long i = 0; i < N; i++) { // i日目
+        for (long long h = 1; h <= H + N*B; h++) { // 満腹度h
+            if (dp[i][h] == INF) continue; // i日目に満腹度がhであることは不可能
+
+            dp[i+1][h+B] = min(dp[i+1][h+B], dp[i][h] + A);
+            dp[i+1][h+D] = min(dp[i+1][h+D], dp[i][h] + C);
+            if (h > E) { // h-E > 0なら
+                dp[i+1][h-E] = min(dp[i+1][h-E], dp[i][h]);
+            }
+        }
     }
+
+    long long ans = INF;
+    for (long long h = 1; h <= H + N*B; h++) {
+        ans = min(ans, dp[N][h]);
+    }
+    cout << ans << endl;
 }
