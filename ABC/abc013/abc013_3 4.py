c2deaffe42da5f2fd1ddacee1c31b12fb4c1c5cb
plumandcola <namite0207@gmail.com>
2026-08-23 23:21:28 +0900

    Refactor cost calculation and condition check

--- ABC/abc013/abc013_3 4.py
+++ ABC/abc013/abc013_3 4.py
@@ -5,9 +5,7 @@ A, B, C, D, E = map(int, input().split())
 ans = float("inf")
 for i in range(N+1): #普通の食事をとる日数
     for j in range(N-i+1): #質素な食事をとる日数
-        ans_ij = A * i + C * j #食費の合計
-        s = H + B * i + D * j - E * (N - i - j) #満腹度
-        if s > 0:
-            ans = min(ans, ans_ij)
+        if H + B * i + D * j - E * (N - i - j) > 0: #満腹度が0以下にならないようにできる
+            ans = min(ans, A * i + C * j)
 
 print(ans)
