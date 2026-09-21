30e7286574a9b829c6e0b1413b9b6aeb571998af
plumandcola <namite0207@gmail.com>
2026-09-21 18:02:43 +0900

    Refactor least prime factors to smallest prime factor

--- ABC/abc254/abc254_d 4.py
+++ ABC/abc254/abc254_d 4.py
@@ -1,15 +1,15 @@
 N = int(input())
 
-least_prime_factors = list(range(N+1)) #最小素因数
+spf = list(range(N+1)) #spf = smallest prime factor = 最小素因数
 for p in range(2, N+1):
     for i in range(2 * p, N+1, p):
-        least_prime_factors[i] = min(least_prime_factors[i], p)
+        spf[i] = min(spf[i], p)
 
 ans = 0
 for i in range(1, N+1):
     m = 1 #平方数にするために掛けなければいけない最小の数
     while i != 1:
-        j = least_prime_factors[i]
+        j = spf[i]
         while i % (j * j) == 0:
             i //= j * j
         if i % j == 0: #iを素因数jで割れる回数が奇数回
