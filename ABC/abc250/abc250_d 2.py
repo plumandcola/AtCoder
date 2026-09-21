a77fb152daf60e2e84260d699778065173785384
plumandcola <namite0207@gmail.com>
2026-09-21 19:39:02 +0900

    Fix binary search and prime generation logic

--- ABC/abc250/abc250_d 2.py
+++ ABC/abc250/abc250_d 2.py
@@ -3,10 +3,10 @@ N = int(input())
 #二分探索で、Nの3乗根(切り上げ)を求める
 l = 0
 r = 10**6 + 1
-while l != r:
-    mid = (l+r) // 2
+while r - l > 1:
+    mid = (l + r) // 2
     if mid ** 3 < N:
-        l = mid + 1
+        l = mid
     else:
         r = mid
 
@@ -19,7 +19,7 @@ primes = []
 for n in range(2, N_cbrt+1):
     if is_prime[n]: #nが素数なら
         primes.append(n)
-        for i in range(2*n, N_cbrt+1, n):
+        for i in range(n*n, N_cbrt+1, n):
             is_prime[i] = False
 
 ans = 0
